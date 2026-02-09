e5ac97eb24968ff6b060b1717bba1c4ae89d7516
muliroZ <murilomega100@gmail.com>
2026-02-09 01:48:02 -0300

    spring boot project structure implemented

---
+++ src/main/java/dev/muliroz/financial_ledger/FinancialLedgerApplication.java
@@ -0,0 +1,13 @@
+package dev.muliroz.financial_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialLedgerApplication.class, args);
+	}
+
+}
