d3c41be8a851c9001b7e827eb61046add7f0b12a
muliroZ <murilomega100@gmail.com>
2026-02-09 01:48:02 -0300

    spring boot project structure implemented

---
+++ src/test/java/dev/muliroz/financial_ledger/FinancialLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.muliroz.financial_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
