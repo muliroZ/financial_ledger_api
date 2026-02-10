21ff8385942016ff64552c8be36ffe737eac7571
muliroZ <murilomega100@gmail.com>
2026-02-09 23:30:32 -0300

    exception handling optimization

---
+++ src/main/java/dev/muliroz/financial_ledger/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package dev.muliroz.financial_ledger.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IdempotencyKeyException.class)
+    public ResponseEntity<?> handleIdempotencyKeyException(IdempotencyKeyException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<?> handleInsufficientBalanceException(InsufficientBalanceException ex) {
+        return ResponseEntity.status(HttpStatus.PAYMENT_REQUIRED)
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(WalletAlreadyRegisteredException.class)
+    public ResponseEntity<?> handleWalletAlreadyRegisteredException(WalletAlreadyRegisteredException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(ex.getMessage());
+    }
+}
