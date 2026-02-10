b529d90f971d8bfaf877f1dc12d8443a18706199
muliroZ <murilomega100@gmail.com>
2026-02-09 23:31:22 -0300

    controller layer and additions on service layer + utils

---
+++ src/main/java/dev/muliroz/financial_ledger/controller/WalletController.java
@@ -0,0 +1,59 @@
+package dev.muliroz.financial_ledger.controller;
+
+import dev.muliroz.financial_ledger.dto.BalanceResponse;
+import dev.muliroz.financial_ledger.model.Wallet;
+import dev.muliroz.financial_ledger.service.WalletService;
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/wallets")
+public class WalletController {
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @GetMapping("/{cpfCnpj}")
+    public ResponseEntity<Wallet> showWallet(@PathVariable String cpfCnpj) {
+        if (cpfCnpj.length() > 14) throw new RuntimeException();
+
+        Wallet wallet = walletService.findWalletByCpfCnpj(cpfCnpj);
+
+        return ResponseEntity.status(200)
+                .contentType(MediaType.APPLICATION_JSON)
+                .lastModified(ZonedDateTime.now())
+                .cacheControl(
+                        CacheControl.maxAge(Duration.ofHours(12))
+                                .mustRevalidate()
+                                .cachePrivate()
+                )
+                .body(wallet);
+    }
+
+    @GetMapping("/{id}/balance")
+    public ResponseEntity<BalanceResponse> getBalance(@PathVariable UUID id) {
+        BalanceResponse response = walletService.getBalance(id);
+        return ResponseEntity.status(200)
+                .contentType(MediaType.APPLICATION_JSON)
+                .lastModified(ZonedDateTime.now())
+                .cacheControl(
+                        CacheControl.maxAge(Duration.ofDays(1))
+                                .mustRevalidate()
+                                .cachePrivate()
+                )
+                .body(response);
+    }
+}
