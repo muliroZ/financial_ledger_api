097c3e89806007e9f95dbe9e56d519bc848e9bab
muliroZ <murilomega100@gmail.com>
2026-02-09 21:11:43 -0300

    service layer and initial exceptions implemented

---
+++ src/main/java/dev/muliroz/financial_ledger/exception/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package dev.muliroz.financial_ledger.exception;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
